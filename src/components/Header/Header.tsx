23f3ffbd85231f4189f32d835bcdd4cf62edf98e
Alexandre Perreau <alex.perreau@hotmail.fr>
2025-01-19 22:37:40 +0100

    HTML CSS

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import { Link, useLocation } from 'react-router-dom'
+import argentBankLogo from "../../assets/argentBankLogo.png"
+
+function Header() {
+  const location = useLocation()
+
+  function links() {
+    switch (location.pathname) {
+      case '/User':
+        return (
+          <>
+            <Link to="/User" className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              Tony
+            </Link>
+            <Link to="/" className="main-nav-item">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </>
+        )
+      default:
+        return (
+          <Link to="/SignIn" className="main-nav-item">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )
+    }
+  }
+
+  return (
+    <header>
+      <nav className="main-nav">
+        <Link to="/" className="main-nav-logo">
+          <img
+            className="main-nav-logo-image"
+            src={argentBankLogo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>{links()}</div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+import './main.css'
+import Header from './components/Header/Header.tsx'
+import Footer from './components/Footer/Footer.tsx'
+import Home from './pages/Home/Home.tsx'
+import SignIn from './pages/SignIn/SignIn.tsx'
+import User from './pages/User/User.tsx'
+// import Error from './pages/Error/Error.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
+        {/* <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Navigate to="/error" replace={true} />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
+  </StrictMode>
+)
